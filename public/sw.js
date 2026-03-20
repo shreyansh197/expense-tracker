00b7ba9267df51c7c94a0ad826e05205a93d1389
shreyansh197 <shreyansh19july@gmail.com>
2026-03-20 11:51:43 +0530

    Fix PWA manifest icons and service worker for install prompt

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,6 @@
 const CACHE_NAME = "expense-tracker-v1";
-const STATIC_ASSETS = ["/", "/expenses", "/settings"];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)),
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
